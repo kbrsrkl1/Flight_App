e2788f8c735976d62a2e80e328e76aefc1828265
kbrsrkl1 <kubranur216@gmail.com>
2023-04-01 18:04:34 +0300

    commit

--- flight/serializers.py
+++ flight/serializers.py
@@ -32,4 +32,33 @@ class ReservationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Reservation
-        fields = ("id", "flight", "flight_id", "passenger","user")
+        fields = ("id", "flight", "flight_id", "user", "passenger")
+
+    def create(self, validated_data):
+        passenger_data = validated_data.pop("passenger")
+        validated_data["user_id"] = self.context["request"].user.id
+        reservation = Reservation.objects.create(**validated_data)
+
+        for passenger in passenger_data:
+            pas = Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+
+        reservation.save()
+        return reservation
+    
+class StaffFlightSerializer(serializers.ModelSerializer):
+
+    reservation = ReservationSerializer(many=True, read_only=True)
+
+    class Meta:
+            model = Flight
+            fields = (
+            "id",
+            "flight_number",
+            "operation_airlines",
+            "departure_city",
+            "arrival_city",
+            "date_of_departure",
+            "etd",
+            "reservation"
+        )
--- flight/views.py
+++ flight/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import FlightSerializer, ReservationSerializer
+
+from users import serializers
+from .serializers import FlightSerializer, ReservationSerializer, StaffFlightSerializer
 from .models import Flight, Reservation
 from rest_framework.permissions import IsAdminUser
 from .permissions import IsStafforReadOnly
+from datetime import datetime, date
 
 
 
@@ -12,6 +15,40 @@ class FlightView(viewsets.ModelViewSet):
     serializer_class = FlightSerializer
     permission_classes = [IsStafforReadOnly]
 
+
+    def get_serializer_class(self):
+        serializer =  super().get_serializer_class()
+
+        if self.request.user.is_staff:
+            return StaffFlightSerializer
+    
+        return serializer
+    
+    def get_queryset(self):
+        now = datetime.now()
+        current_time = now.strftime('%H:%M:%S')
+        today = date.today()
+
+        if self.request.user.is_staff:
+            return super().get_queryset()
+        
+        else:
+            queryset = Flight.objects.filter(date_of_deparature__gt=today)
+
+            if Flight.objects.filter(date_of_deparature__gt=today):
+                today_qs = Flight.objects.filter(date_of_deparature__gt=today).filter(etd__gt=current_time)
+
 class ReservationView(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        
+
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(users = self.request.user)
+        
+
+
