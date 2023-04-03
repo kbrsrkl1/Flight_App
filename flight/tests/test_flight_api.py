68526c36da72acf6275e63286a69b163c5ff7c2f
kbrsrkl1 <kubranur216@gmail.com>
2023-04-03 18:16:30 +0300

    commit

--- flight/tests/test_flight_api.py
+++ flight/tests/test_flight_api.py
@@ -4,19 +4,22 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
 from flight.models import Flight
 from flight.views import FlightView
+from datetime import datetime, date
 
 class FlightTestCase(APITestCase):
-
+    today = date.today()
+    now = datetime.now()
+    current_time = now.strftime('%H:%M:%S')
 
     def setUp(self):
         self.factory = APIRequestFactory()
         self.flight = Flight.objects.create(
-            flight_number = '123ABC',
-            operation_airlines =  'THY',
-            departure_city = 'ADANA',
-            arrival_city = 'ANKARA',
-            date_of_departure = '2023-04-29',
-            etd = '18:35:00'
+            flight_number='123ABC',
+            operation_airlines='THY',
+            departure_city='ADANA',
+            arrival_city='ANKARA',
+            date_of_departure=f'{self.today}',
+            etd=f'{self.current_time}'
         )
         self.user = User.objects.create_user(
             username='admin',
@@ -26,19 +29,19 @@ class FlightTestCase(APITestCase):
 
     def test_flight_lis_as_non_auth_user(self):
         request = self.factory.get('/flight/flights/')
-        #print(reverse('flights-list'))
-        response = FlightView.as_view({'get' : 'list'})(request)
-        print(response)
-        self.assertEquals(response.status_code, 200)
-    
+        response = FlightView.as_view({'get': 'list'})(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'reservation')
+        self.assertEqual(len(response.data), 0)
 
 
     def test_flight_list_as_staff_user(self):
-        request = self.factory.get('/flight/flights/', HTTP_AUTHORİZATİON = f'Token {self.token}')
-        self.user.is_staff=True
+        request = self.factory.get('/flight/flights/', HTTP_AUTHORİZATİON=f'Token {self.token}')
+        self.user.is_staff = True
         self.user.save()
         force_authenticate(request, user=self.user)
         request.user = self.user
-        response = FlightView.as_view({'get' : 'list'})(request)
+        response = FlightView.as_view({'get': 'list'})(request)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'reservation')
+        self.assertEqual(len(response.data), 1)
